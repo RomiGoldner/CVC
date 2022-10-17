ae3cf28e337deafd7b893c9353b996af48010b97
Romi Goldner <romigoldner@gmail.com>
2022-10-17 15:27:01 +0300

    Updates

--- scripts/download_cvc_training_data.py
+++ scripts/download_cvc_training_data.py
@@ -2,14 +2,14 @@
 import os
 import gdown
 
-def download_cvc_model():
+def download_cvc_train_data():
     # download the model
-    dir_url = "https://drive.google.com/drive/folders/1R4sm0TLl6sHB4Ge4o2m0pPasKhva3T8s?usp=sharing"
+    dir_url = "https://drive.google.com/file/d/1hWP_d3NFUZVPUyiZsumPo7qNTEQl_pIY/view?usp=sharing"
     # directory to save the model
-    output = 'output_5mil_even_priv_pub'
+    output = 'data'
 
     # download the whole directory from dir_url
     gdown.download_folder(dir_url,  quiet=False)
 
 if __name__ == "__main__":
-    download_cvc_model()
+    download_cvc_train_data()
